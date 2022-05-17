6d803636946475d0a5854da082997acce7a0a271
Noé Domenech de Cellès <noe.domenechdecelles@ringcentral.com>
2022-05-17 15:54:34 +0200

    Attempt to handle request_thread_control

---
+++ lib/facebook/messenger/incoming/request_thread_control.rb
@@ -0,0 +1,22 @@
+module Facebook
+  module Messenger
+    module Incoming
+      # The RequestThreadControl class represents an incoming Facebook Messenger
+      # request thread control event.
+      #
+      # @see https://developers.facebook.com/docs/messenger-platform/handover-protocol/request-thread-control
+      # @see https://developers.facebook.com/docs/messenger-platform/reference/handover-protocol/request-thread-control
+      class RequestThreadControl
+        include Facebook::Messenger::Incoming::Common
+
+        def new_owner_app_id
+          @messaging['request_thread_control']['new_owner_app_id']
+        end
+
+        def metadata
+          @messaging['request_thread_control']['metadata']
+        end
+      end
+    end
+  end
+end
